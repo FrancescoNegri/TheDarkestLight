c7170d6665ba3224ac15b9be481b3f6f5e150c40
unknown <cacioppino98@gmail.com>
2018-09-26 21:17:36 +0800

    Funtion find name of file from path in Room.js vers2

--- www/lib/rooms/Room.js
+++ www/lib/rooms/Room.js
@@ -43,9 +43,10 @@ class Room extends Phaser.Scene {
     }
 
     findFileNameFromPath(path){
-        var nameFirstCharachterPosition = path.lastIndexOf("/") +1;
+        var nameFirstCharachterPosition= path.lastIndexOf("/") +1;
         var nameLastCharachterPosition = path.lastIndexOf(".");
-        if (nameFirstCharachterPosition == -1 || nameLastCharachterPosition== -1) return -1;
+        if (nameFirstCharachterPosition == -1) nameFirstCharachterPosition=0;
+        if (nameLastCharachterPosition== -1) return -1;
         var filename= path.slice(nameFirstCharachterPosition,nameLastCharachterPosition);
         return filename;
     }

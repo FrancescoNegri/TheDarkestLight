712dda8339d0a4bcaf3be6f0dc1e30fd9d39eed3
FrancescoNegri <francesco.negri@outlook.com>
2018-10-30 13:30:00 +0800

    fix

--- www/js/characters/player/Debugger.js
+++ www/js/characters/player/Debugger.js
@@ -5,7 +5,6 @@ class Debugger extends Player {
 
     create() {
         super.create();
-        this.setCollideWorldBounds(true); //.setPipeline('Light2D');
 
         let config = {
             key: 'walk',
@@ -26,20 +25,5 @@ class Debugger extends Player {
 
     update() {
         super.update();
-        /*this.body.setVelocity(0);
-
-        if (this.room.cursors.left.isDown) {
-            this.body.setVelocityX(-300);
-        }
-        else if (this.room.cursors.right.isDown) {
-            this.body.setVelocityX(300);
-        }
-
-        if (this.room.cursors.up.isDown) {
-            this.body.setVelocityY(-300);
-        }
-        else if (this.room.cursors.down.isDown) {
-            this.body.setVelocityY(300);
-        }*/
     }
 }

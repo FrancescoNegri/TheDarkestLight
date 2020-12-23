a80a147bf911af72d51230132e4eac6840eee985
FrancescoNegri <francesco.negri.98@gmail.com>
2020-12-23 16:49:47 +0100

    Fix screens

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,5 +1,4 @@
-import TDLib, { Settings } from '../../lib/the-darkest-lib';
-import Phaser from 'phaser';
+import TDLib from '../../lib/the-darkest-lib';
 import TestScreen2 from './TestScreen2';
 
 export default class TestScreen extends TDLib.Screens.Screen {
@@ -16,44 +15,20 @@ export default class TestScreen extends TDLib.Screens.Screen {
   create() {
     super.create();
 
-    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    this.keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-    this.keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-    this.keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-
     this.title = this.add.text(0, 0, 'The Darkest Light');
-    this.title.x = Settings.SCREEN_PROPS.calculatedWidth / 2;
-    this.title.y = Settings.SCREEN_PROPS.calculatedHeight / 2;
+    this.title.x = TDLib.Settings.SCREEN_PROPS.calculatedWidth / 2;
+    this.title.y = TDLib.Settings.SCREEN_PROPS.calculatedHeight / 2;
     this.title.setStyle({ fontFamily: 'Arial', fontSize: 64, color: '#00ff00' });
     this.title.setOrigin(0.5);
 
     this.cameras.main.on('camerafadeoutcomplete', function () {
       this.start(TestScreen2);
     }, this);
+    this.cameras.main.fadeOut(1500);
   }
 
   update() {
     super.update();
-
-    if (Phaser.Input.Keyboard.JustDown(this.keyA)) {
-      this.cursors.hideCursor();
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyS)) {
-      this.cursors.showCursor();
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyD)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyF)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyX)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyZ)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyC)) {
-      this.cameras.main.fadeOut(1000);
-    }
+    this.cursors.hideCursor();
   }
 }
--- src/screens/TestScreen2.js
+++ src/screens/TestScreen2.js
@@ -1,5 +1,4 @@
-import TDLib, { Settings } from '../../lib/the-darkest-lib';
-import Phaser from 'phaser';
+import TDLib from '../../lib/the-darkest-lib';
 import TestRoom from '../rooms/TestRoom';
 
 export default class TestScreen2 extends TDLib.Screens.Screen {
@@ -16,45 +15,24 @@ export default class TestScreen2 extends TDLib.Screens.Screen {
   create() {
     super.create();
 
-    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    this.keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-    this.keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-    this.keyC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
-
-    this.cameras.main.fadeIn(2000);
     this.title = this.add.text(0, 0, 'A dark game.');
-    this.title.x = Settings.SCREEN_PROPS.calculatedWidth / 2;
-    this.title.y = Settings.SCREEN_PROPS.calculatedHeight / 2;
+    this.title.x = TDLib.Settings.SCREEN_PROPS.calculatedWidth / 2;
+    this.title.y = TDLib.Settings.SCREEN_PROPS.calculatedHeight / 2;
     this.title.setStyle({ fontFamily: 'Arial', fontSize: 44, color: '#00ff00' });
     this.title.setOrigin(0.5);
 
     this.cameras.main.on('camerafadeoutcomplete', function () {
+      this.cursors.showCursor();
       this.start(TestRoom);
     }, this);
+    this.cameras.main.on('camerafadeincomplete', () => {
+      this.cameras.main.fadeOut(1000);
+    }, this);
+
+    this.cameras.main.fadeIn(1000);
   }
 
   update() {
     super.update();
-
-    if (Phaser.Input.Keyboard.JustDown(this.keyA)) {
-      this.cursors.hideCursor();
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyS)) {
-      this.cursors.showCursor();
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyD)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyF)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyX)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyZ)) {
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.keyC)) {
-      this.cameras.main.fadeOut(1000);
-    }
   }
 }
